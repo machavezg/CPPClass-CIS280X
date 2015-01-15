73496050fd7e6d527df8ecbbfa36bbd83e4f1d0a
Miguel <machavezg@icloud.com>
2015-01-14 19:43:40 -0800

    Create firstclass.cpp

---
+++ firstclass.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int square( int n );
+void cube( int &n );
+void doubleIt(int *n);
+
+int main() {
+   int x, y, z; //varibles are uninitialized. Values are garbage
+   //cout << "x is " << x << "y is " << y << "z is " << z << endl;
+   
+   x = 5;   
+   cout << "The value of " << x << " is " << endl;
+   x = square(x);
+   cout << x << endl; 
+   
+   y = 5;
+   cout << "The square of " << y << " is " << endl;
+   cube(y);
+   cout << y << endl;
+
+   z = 5;
+   cout << "The doubled of " << z << " is " << endl;
+   doubleIt(&z);
+   cout << z << endl;      
+}
+
+int square( int n ){
+   return n * n;
+}
+void cube( int &n) {
+   n*=n*=n;
+}
+void doubleIt(int *n){
+   *n = *n *= 2;
+}
