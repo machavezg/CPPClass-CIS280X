947eb86550817cfb49926adcab51223d21ba955a
Miguel <machavezg@icloud.com>
2015-01-28 19:51:56 -0800

    Create assign1.cpp

---
+++ HW1/assign1.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cstdlib>
+#include <time.h>
+#include <iomanip>
+
+using namespace std;
+
+int double1(int arr);
+void double2(int &arr);
+void double3(int *vect);
+void triple1(int array1[], int size);
+void triple2(int &arr);
+void triple3(int *vect);
+
+
+
+int main() {
+    srand(time(NULL));
+    int size = 10;
+    array <int, 10> array2;
+    int array1[size];
+    vector<int> array3;
+    
+    for(int i = 0; i < size; i++){
+        array1[i] = rand() % 51 + 50;
+        array2[i] = rand() % 51 + 50;
+        array3.push_back(rand() % 51 + 50);
+    }
+//The following code is used to display the first three arrays
+    cout << "Arrays after loading them with random numbers in the range [50,100]:"; 
+    
+    cout << "\n\nArray1:\t";
+    for(int i = 0; i < size; i++){
+        cout << setw(3) << left << array1[i] << "  ";
+    }
+    cout << "\nArray2:\t"; 
+    for(int i = 0; i < size; i++){
+        cout << setw(3) << left << array2[i] << "  "; 
+    }
+    
+    cout << "\nArray3:\t";
+    for(int i = 0; i < array3.size(); i++){
+        cout << setw(3) << left << array3[i] << "  ";
+    }
+   
+//The following code displays the arrays after they have been doubled
+    cout << "\n\nArrays after calling a function element by"
+         << " element to double each element in the arrays:";
+
+    cout << "\n\nArray1:\t";
+    for(int i = 0; i < size; i++){
+        cout << setw(3) << left << double1(array1[i]) << "  ";
+    }
+   
+    cout << "\nArray2:\t";
+    for(int a : array2){
+        double2(a);
+        cout << setw(3) << left << a << "  ";
+    }
+    
+    cout << "\nArray3:\t";
+    for(int v : array3){
+        double3(&v);
+        cout << setw(3) << left << v << "  ";
+    }
+    
+//The following code displays the arrays after they have been doubled
+    cout << "\n\nArrays after passing them to a function"
+         << " to triple each element in the arrays:";
+
+    cout << "\n\nArray1:\t";
+    for(int i = 0; i < size; i++){
+        triple1(array1, i);
+    }
+    for(int i = 0; i < size; i++){
+        cout << setw(3) << left << array1[i] << "  ";
+    }    
+    
+    cout << "\nArray2:\t";
+    for(int a : array2){
+        triple2(a);
+        cout << setw(3) << left << a << "  ";
+    }
+    
+    cout << "\nArray3:\t";
+    for(int v : array3){
+        triple3(&v);
+        cout << setw(3) << left << v << "  ";
+    }
+    
+    cout << "\n\n";
+    system("pause");
+//      cout << "\n\nPress any key to continue...\n\n";
+//      cin.get();
+}
+int double1(int arr){
+    return arr * 2;
+}
+void double2(int &arr){
+    arr *= 2;
+}
+void double3(int *vect){
+    *vect *= 2;
+}
+void triple1(int array1[], int size){
+    array1[size] *= 3;
+}
+void triple2(int &arr){
+    arr *= 3;  
+}
+void triple3(int *vect){
+    *vect *= 3;
+}
