812f38d9ad22885d9832eead573ed71a0aad35ef
Miguel <machavezg@icloud.com>
2015-04-08 18:30:47 -0700

    Update employee.cpp

--- HW3/employee.cpp
+++ HW3/employee.cpp
@@ -2,9 +2,10 @@
 
 #include <iostream>
 #include <stdexcept>
-#include "emplouee.h"
+#include "employee.h"
 using namespace std;
 
+//Constructor
 employee::employee(const string &first, const string &last, double salary)
 {
   firstName = first;
@@ -12,39 +13,46 @@ employee::employee(const string &first, const string &last, double salary)
   setMonthlySalary( salary );
 }
 
+//Set first name
 void employee::setFirstName( const string &first)
 {
   firstName = first;
 }
 
+//Return first name
 string employee::getFirstName() const
 {
   return firstName;
 }
 
+//Set last name
 void employee::setLastName( const string &last)
 {
   lastName = last;
 }
 
+//Return last name
 string employee::getLastName() const
 {
   return lastName;
 }
 
+//Set monthly salary
 void employee::setMonthlySalary( double salary)
 {
-  if( salary >= 0.0 )
+  if( salary >= 0.0 ) //Validate that salary is positive
     monthlySalary = salary;
-  else
+  else //Set salary to 0.0 if it's negative
     monthlySalary = 0.0;
 }
 
+//Return monthly salary
 double employee::getMonthlySalary() const
 {
   return monthlySalary;
 }
 
+//Print employee object
 void employee::displayEmployee() cosnt
 {
   cout << "First Name: " << firstName << "\nLast Name: " << lastName << "\nMonthly Salary: " << monthlySalary;
