419d88788470fdd5cdcf0a48c67388605346e598
Miguel <machavezg@icloud.com>
2015-04-09 01:28:01 -0700

    Update Employee.h

--- HW3/Employee.h
+++ HW3/Employee.h
@@ -4,25 +4,25 @@
 #define EMPLOYEE_H
 
 #include <string>
-using namespace std;
 
-class employee
+class Employee
 {
   public:
-    employee(const string&, const string &, double 0.0);
+    Employee( const std::string &, const std::string &, double = 0.0 );
     
-    void setFirstName( const string & );
-    string getFirstName() const;
+    void setFirstName( const std::string & );
+    std::string getFirstName() const;
     
-    void setLastName( const string & );
-    string getLastName() const;
+    void setLastName( const std::string & );
+    std::string getLastName() const;
     
     void setMonthlySalary( double );
     double getMonthlySalary() const;
     
+    void displayEmployee() const;
   private:
-    string firstName;
-    string lastName;
+    std::string firstName;
+    std::string lastName;
     double monthlySalary;
 };
 
